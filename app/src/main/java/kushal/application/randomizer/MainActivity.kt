0640e192b28aa0585764648b31aace4fa77f8b4a
Kushal-Gera <kushalgera1212@gmail.com>
2019-09-27 22:05:06 +0530

    Initial commit

---
+++ app/src/main/java/kushal/application/randomizer/MainActivity.kt
@@ -0,0 +1,17 @@
+package kushal.application.randomizer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+
+}
