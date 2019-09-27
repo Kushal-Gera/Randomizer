ae662589fe940b914f4e9ca4a69c4894eb6a5957
Kushal-Gera <kushalgera1212@gmail.com>
2019-09-27 23:20:44 +0530

    UI Done

--- app/src/main/java/kushal/application/randomizer/MainActivity.kt
+++ app/src/main/java/kushal/application/randomizer/MainActivity.kt
@@ -2,12 +2,21 @@ package kushal.application.randomizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
+import android.widget.BaseAdapter
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ll1.setOnClickListener {
+
+        }
 
 
 
